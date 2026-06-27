ffcc514a791ee9cb247c2e3ede3742dcf82120c4
Viktor Shcherbak <dev.psychologist@gmail.com>
2026-06-27 22:01:19 +0300

    Books models, views, serializers, test created

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from books.models import Book
+from books.permissions import IsAdminOrReadOnly
+from books.serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    """
+    POST    /api/books/        - додати нову книгу (тільки staff)
+    GET     /api/books/        - список книг (всім, включно з анонімними)
+    GET     /api/books/<id>/   - деталі книги (всім)
+    PUT/PATCH /api/books/<id>/ - оновити книгу, в т.ч. inventory (тільки staff)
+    DELETE  /api/books/<id>/   - видалити книгу (тільки staff)
+    """
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOnly,)
